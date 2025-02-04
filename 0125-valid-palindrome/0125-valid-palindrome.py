e4fa2e2292cc62d4c96024f26fe88bfa09e433a6
Jay Parmar <90332067+arrenndajo@users.noreply.github.com>
2025-02-04 16:56:19 +0530

    Time: 19 ms (9.87%), Space: 18.2 MB (52.28%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,28 +1,32 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # faster solution which can't be used in interviews
-        newStr = ""
-
-        for c in s:
-            if c.isalnum():
-                newStr += c.lower()
-        return newStr == newStr[::-1]
-
 
     # slower but better solution:
-    #     l, r = 0, len(s) - 1
+    # 27ms runtime and TC of O(N).
+    
+        l, r = 0, len(s) - 1
 
-    #     while l < r:
-    #         while l < r and not self.alphaNum(s[l]):
-    #             l += 1
-    #         while r > l and not self.alphaNum(s[r]):
-    #             r -= 1
-    #         if s[l].lower() != s[r].lower():
-    #             return False
-    #         l, r = l + 1, r - 1
-    #     return True
+        while l < r:
+            while l < r and not self.alphaNum(s[l]):
+                l += 1
+            while r > l and not self.alphaNum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l, r = l + 1, r - 1
+        return True
 
-    # def alphaNum(self, c):
-    #     return (ord('A') <= ord(c) <= ord('Z') or
-    #             ord('a') <= ord(c) <= ord('z') or
-    #             ord('0') <= ord(c) <= ord('9'))
+    def alphaNum(self, c):
+        return (ord('A') <= ord(c) <= ord('Z') or
+                ord('a') <= ord(c) <= ord('z') or
+                ord('0') <= ord(c) <= ord('9'))
+
+        # faster solution which can't be used in interviews as it uses inbuilt functions:
+        # 11ms runtime and TC of O(N).
+
+        # newStr = ""
+
+        # for c in s:
+        #     if c.isalnum():
+        #         newStr += c.lower()
+        # return newStr == newStr[::-1]
