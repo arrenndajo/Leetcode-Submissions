69200adceccd1bddae99b11a5d44621bf5bc6ad9
Jay Parmar <90332067+arrenndajo@users.noreply.github.com>
2025-03-26 17:27:39 +0530

    Time: 19 ms (87.14%), Space: 23.3 MB (79.45%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,15 +1,24 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        output = [1] * len(nums)
-        
-        left = 1
+        res = [1] * (len(nums))
+
+        prefix = 1
         for i in range(len(nums)):
-            output[i] *= left
-            left *= nums[i]
-        
-        right = 1
-        for i in range(len(nums) - 1, -1, -1):
-            output[i] *= right
-            right *= nums[i]
-    
-        return output        
+            res[i] = prefix
+            prefix *= nums[i]
+
+        postfix = 1
+        for i in range(len(nums) -1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
+
+# Let's change 1, 2, 3, 4 positions to symbols like a, b, c, d. So multiplying will be:
+# Prefix:
+# -> moves from left to right.
+# |    a    |  a*b  | a*b*c | a*b*c*d |
+# postfix:
+# <- moves from right to left.
+# | a*b*c*d | b*c*d |  c*d  |    d    |
+# The RESULT is a multiply without the symbol in own position (the left value from prefix and the right one from postfix):
+# |  b*c*d  | a*c*d | a*b*d |  a*b*c  |
