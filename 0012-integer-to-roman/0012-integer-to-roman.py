9e9a2bb51d5316b155391cddda9aa133d2ce1b59
Jay Parmar <90332067+arrenndajo@users.noreply.github.com>
2025-03-29 17:21:42 +0530

    Time: 1 ms (86.81%), Space: 18 MB (6.72%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -4,6 +4,7 @@ class Solution:
                     ["X", 10], ["XL", 40], ["L", 50], ["XC", 90],
                     ["C", 100], ["CD", 400], ["D", 500], ["CM", 900],
                     ["M", 1000]]
+                    
         result = "" 
         for key, val in reversed(keyList):
             if num // val != 0: 
