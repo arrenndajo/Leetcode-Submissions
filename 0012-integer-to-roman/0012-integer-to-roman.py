9692385e8702e4628feff67da93f9fd1860bbee7
Jay Parmar <90332067+arrenndajo@users.noreply.github.com>
2025-03-29 17:21:15 +0530

    Time: 3 ms (79.18%), Space: 17.8 MB (70.87%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,14 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        keyList = [["I", 1], ["IV", 4], ["V", 5], ["IX", 9],
+                    ["X", 10], ["XL", 40], ["L", 50], ["XC", 90],
+                    ["C", 100], ["CD", 400], ["D", 500], ["CM", 900],
+                    ["M", 1000]]
+        result = "" 
+        for key, val in reversed(keyList):
+            if num // val != 0: 
+                # eg: 1000/1000 = 1, it gets added to count.
+                count = num // val
+                result += (key * count)
+                num = num % val
+        return result
